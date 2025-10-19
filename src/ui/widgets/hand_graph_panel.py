56970790aa7adee7420d2a2205bff2994d0908cb
hermit71 <v.koloshko@gmail.com>
2025-10-19 19:00:47 +0300

    PlotGraph: New Widget for Plotting

--- src/ui/widgets/hand_graph_panel.py
+++ src/ui/widgets/hand_graph_panel.py
@@ -1,26 +1,49 @@
 """Panel containing multiple real-time graphs."""
 
-from PyQt6.QtWidgets import QFrame, QWidget
+from PyQt6.QtWidgets import QFrame, QWidget, QVBoxLayout
 from PyQt6.QtCore import pyqtSlot as Slot
 
 from src.ui.widgets.graph_widget import GraphWidget
 from src.ui.widgets.time_series_plot_widget import TimeSeriesPlotWidget
 
+X_AXIS_RANGE = 30.0
+POINTS_PER_WINDOW = 1200
+
 class PlotPanel(QFrame):
     def __init__(self, parent: QWidget, data_source):
         super().__init__(parent)
         self.parent = parent
+        self.vbox = QVBoxLayout()
         self.data_source = data_source
-        self.plots = [TimeSeriesPlotWidget() for _ in range(2)]
+        self.plt_torque = TimeSeriesPlotWidget(
+        x_window_seconds=X_AXIS_RANGE,
+        points_per_window=POINTS_PER_WINDOW,
+        y_range=(-50.0, 50.0),
+        background="w",
+        antialias=True,
+        with_legend=True,
+        )
+        self.plt_velocity = TimeSeriesPlotWidget(
+            x_window_seconds=X_AXIS_RANGE,
+            points_per_window=POINTS_PER_WINDOW,
+            y_range=(-5.0, 5.0),
+            background="w",
+            antialias=True,
+            with_legend=True,
+        )
+        self.setup_ui()
 
     def setup_ui(self):
-        for plot in self.plots:
-            plot.set_axis_labels(y_label='FFF')
+        self.vbox.addWidget(self.plt_torque)
+        self.vbox.addWidget(self.plt_velocity)
+        self.setLayout(self.vbox)
+
+        self.plt_torque.set_axis_labels(y_label="Крутящий момент, Нм")
+        self.plt_velocity.set_axis_labels(y_label='Скорость изменения, Нм/с')
 
 
 class HandGraphPanel(QFrame):
     """Widget arranging and updating graph widgets."""
-T
     def __init__(self, parent=None):
         super().__init__(parent)
         self.model = None
